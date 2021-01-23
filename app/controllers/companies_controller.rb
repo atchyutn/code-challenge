a7d6004e83d1c5dab0f7642fc5a62bd6dc30fa52
Atchyut Nagabhairava <atchyutnagabhairava@gmail.com>
2021-01-23 22:49:23 +0530

    Title: Email validation
    desc: added custom validation for the email using js, model

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -17,6 +17,7 @@ class CompaniesController < ApplicationController
     if @company.save
       redirect_to companies_path, notice: "Saved"
     else
+      flash[:error] = @company.errors.full_messages.join(", ")
       render :new
     end
   end
@@ -28,6 +29,7 @@ class CompaniesController < ApplicationController
     if @company.update(company_params)
       redirect_to companies_path, notice: "Changes Saved"
     else
+      flash[:error] = @company.errors.full_messages.join(", ")
       render :edit
     end
   end  
--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,5 @@
 class Company < ApplicationRecord
   has_rich_text :description
-
+  validates :email, format: { with: /\b[A-Z0-9._%a-z\-]+@getmainstreet\.com\z/,  message: "email should end with @getmainstreet.com"  }, allow_blank: true
 
 end
